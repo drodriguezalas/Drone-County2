d446a608b016a818d35335e4114c580d5c29c93e
drodriguezalas <drodriguezalas@gmail.com>
2018-05-05 13:34:40 -0600

    Working GUI

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -8,18 +8,27 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import javax.swing.JSpinner;
+import javax.swing.JList;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+import java.awt.Color;
 
 public class Gui extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField tFCantidadEstaciones;
+	private JTextField tFAltoPista;
+	private JTextField tFAnchoPista;
+	private JTextField tFCantidadViajes;
+	private JTextField tFTiempoSimulacion;
+	private JTextField tFHoraSegundos;
+	private JTextField tFPistasEstacion;	
 
 	/**
 	 * Launch the application.
@@ -42,7 +51,7 @@ public class Gui extends JFrame {
 	 */
 	public Gui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 530, 385);
+		setBounds(100, 100, 301, 386);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -80,51 +89,116 @@ public class Gui extends JFrame {
 		lblAlgoritmoAUtilizar.setBounds(10, 186, 123, 14);
 		contentPane.add(lblAlgoritmoAUtilizar);
 		
-		textField = new JTextField();
-		textField.setBounds(143, 8, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		ArrayList<JTextField> textFields = new ArrayList<>();
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(143, 33, 86, 20);
-		contentPane.add(textField_1);
+		tFCantidadEstaciones = new JTextField("6");
+		tFCantidadEstaciones.setBounds(143, 8, 86, 20);
+		contentPane.add(tFCantidadEstaciones);
+		tFCantidadEstaciones.setColumns(10);
+		textFields.add(tFCantidadEstaciones);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(143, 58, 86, 20);
-		contentPane.add(textField_2);
+		tFAltoPista = new JTextField("500");
+		tFAltoPista.setColumns(10);
+		tFAltoPista.setBounds(143, 33, 86, 20);
+		contentPane.add(tFAltoPista);
+		textFields.add(tFAltoPista);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(143, 83, 86, 20);
-		contentPane.add(textField_3);
+		tFAnchoPista = new JTextField("200");
+		tFAnchoPista.setColumns(10);
+		tFAnchoPista.setBounds(143, 58, 86, 20);
+		contentPane.add(tFAnchoPista);
+		textFields.add(tFAnchoPista);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(143, 108, 86, 20);
-		contentPane.add(textField_4);
+		tFCantidadViajes = new JTextField("100");
+		tFCantidadViajes.setColumns(10);
+		tFCantidadViajes.setBounds(143, 83, 86, 20);
+		contentPane.add(tFCantidadViajes);
+		textFields.add(tFCantidadViajes);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(143, 133, 86, 20);
-		contentPane.add(textField_5);
+		tFTiempoSimulacion = new JTextField("120");
+		tFTiempoSimulacion.setColumns(10);
+		tFTiempoSimulacion.setBounds(143, 108, 86, 20);
+		contentPane.add(tFTiempoSimulacion);
+		textFields.add(tFTiempoSimulacion);
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(143, 158, 86, 20);
-		contentPane.add(textField_6);
+		tFHoraSegundos = new JTextField("6");
+		tFHoraSegundos.setColumns(10);
+		tFHoraSegundos.setBounds(143, 133, 86, 20);
+		contentPane.add(tFHoraSegundos);
+		textFields.add(tFHoraSegundos);
 		
-		JButton btnBacktraking = new JButton("Backtraking");
-		btnBacktraking.setBounds(10, 211, 123, 23);
-		contentPane.add(btnBacktraking);
+		tFPistasEstacion = new JTextField("2");
+		tFPistasEstacion.setColumns(10);
+		tFPistasEstacion.setBounds(143, 158, 86, 20);
+		contentPane.add(tFPistasEstacion);
+		textFields.add(tFPistasEstacion);
 		
-		JButton btnDivideConquer = new JButton("Divide y venceras");
-		btnDivideConquer.setBounds(10, 245, 123, 23);
-		contentPane.add(btnDivideConquer);
+		JRadioButton rdbtnBacktraking = new JRadioButton("Backtraking");
+		rdbtnBacktraking.setBounds(10, 207, 140, 23);
+		contentPane.add(rdbtnBacktraking);
 		
-		JButton btnProbabilistic = new JButton("Probabilista");
-		btnProbabilistic.setBounds(10, 279, 123, 23);
-		contentPane.add(btnProbabilistic);
-	}
+		JRadioButton rdbtnDidiveYConquista = new JRadioButton("Didive y Conquista");
+		rdbtnDidiveYConquista.setBounds(10, 233, 140, 23);
+		contentPane.add(rdbtnDidiveYConquista);
+		
+		JRadioButton rdbtnProbabilistico = new JRadioButton("Probabilistico");
+		rdbtnProbabilistico.setBounds(10, 259, 140, 23);
+		contentPane.add(rdbtnProbabilistico);
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnBacktraking);
+		group.add(rdbtnDidiveYConquista);
+		group.add(rdbtnProbabilistico);
+		
+		rdbtnBacktraking.setActionCommand("backtraking");
+		rdbtnDidiveYConquista.setActionCommand("divideconquista");
+		rdbtnProbabilistico.setActionCommand("probabilistico");
+		
+		JLabel lblNope = new JLabel("NOPE");
+		lblNope.setEnabled(true);
+		lblNope.setForeground(Color.RED);
+		lblNope.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNope.setBounds(163, 186, 46, 14);
+		lblNope.setVisible(false);
+		contentPane.add(lblNope);
+		
+		JButton btnSiguiente = new JButton("Siguiente");
+		btnSiguiente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				boolean allMarked = true;
+				for(JTextField tf : textFields)
+				{
+					if(tf.getText().isEmpty())
+					{
+						allMarked = false;
+					}
+				}
+				if(allMarked && Integer.parseInt(tFCantidadEstaciones.getText()) <= 30 && Integer.parseInt(tFAltoPista.getText()) <= 500 &&
+						(rdbtnBacktraking.isSelected() || rdbtnDidiveYConquista.isSelected() || rdbtnProbabilistico.isSelected()))
+				{	
+					try
+					{
+						int cantidadEstaciones = Integer.parseInt(tFCantidadEstaciones.getText());
+						int altoPista = Integer.parseInt(tFAltoPista.getText());
+						int anchoPista = Integer.parseInt(tFAnchoPista.getText());
+						int cantidadViajes = Integer.parseInt(tFCantidadViajes.getText());
+						int tiempoSimulacion = Integer.parseInt(tFTiempoSimulacion.getText());
+						int horaSegundos = Integer.parseInt(tFHoraSegundos.getText());
+						int pistasEstacion = Integer.parseInt(tFPistasEstacion.getText());
+						
+						String selection = group.getSelection().getActionCommand();
+						
+						System.out.println(selection);
+					}
+					catch(Exception ex)
+					{
+						lblNope.setVisible(true);
+					}					
+				}
+			}
+		});
+		btnSiguiente.setBounds(91, 313, 89, 23);
+		contentPane.add(btnSiguiente);				
+	}	
 }
---
+++ src/view/SimulationGUI.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class SimulationGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void newScreen() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SimulationGUI frame = new SimulationGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SimulationGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
