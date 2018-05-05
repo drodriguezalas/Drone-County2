9d574de1eb77711e3775e0d267c095fae4c9b19a
Yelson <yelsonmonge@gmail.com>
2018-05-04 23:36:56 -0600

    TripsGenerator 2.0

--- src/controller/TripsGenerator.java
+++ src/controller/TripsGenerator.java
@@ -18,29 +18,32 @@ public class TripsGenerator implements CONSTANTS{
 		controller = Controller.getInstance();
 	}
 	
-	public void generateTrips(int roadWidth, int roadHigh, int tripQuantity ) {
-		int droneQuantityX = roadWidth / DRONE_WIDTH;
-		int droneQuantityY = roadHigh  / DRONE_HIGH;
+	public void generateTrips(int pRoadWidth, int pRoadHigh, int pTripQuantity ) {
+		int droneQuantityX = pRoadWidth / DRONE_WIDTH;
+		int droneQuantityY = pRoadHigh  / DRONE_HIGH;
 		int droneQuantityPerTrip = droneQuantityX * droneQuantityY;
 		
 		DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(controller.getGraphGenerator().getGraph());
 		
-		while(tripQuantity >= droneQuantityPerTrip) {
-			List<Vertex> vertexes = controller.getGraphGenerator().getGraph().getVertexes(); 
-			Collections.shuffle(vertexes);
-			Vertex source = vertexes.get(0);
-			Vertex destination = vertexes.get(1);
-			
-			ArrayList<Vertex> tripVertexes= convertToArrayList(dijkstra.calculateRoad(source, destination));
-			ArrayList<Edge> tripEdges = controller.getGraphGenerator().getGraph().findEdgesForVertexList(tripVertexes);
-			Trip trip = new Trip(tripVertexes, tripEdges, droneQuantityPerTrip);
-			this.tripList.add(trip);
-			
-			tripQuantity -= droneQuantityPerTrip;
+		while(pTripQuantity >= droneQuantityPerTrip) {
+			generateTrips_aux(droneQuantityPerTrip, dijkstra);
+			pTripQuantity -= droneQuantityPerTrip;
 		}
-		if (tripQuantity < 0) {
-			
+		if (pTripQuantity > 0) {
+			generateTrips_aux(pTripQuantity, dijkstra);
 		}
+	}
+	
+	public void generateTrips_aux(int pDroneQuantityPerTrip, DijkstraAlgorithm pDijkstra) {
+		List<Vertex> vertexes = controller.getGraphGenerator().getGraph().getVertexes(); 
+		Collections.shuffle(vertexes);
+		Vertex source = vertexes.get(0);
+		Vertex destination = vertexes.get(1);
+		
+		ArrayList<Vertex> tripVertexes= convertToArrayList(pDijkstra.calculateRoad(source, destination));
+		ArrayList<Edge> tripEdges = controller.getGraphGenerator().getGraph().findEdgesForVertexList(tripVertexes);
+		Trip trip = new Trip(tripVertexes, tripEdges, pDroneQuantityPerTrip);
+		this.tripList.add(trip);
 	}
 	
 	public ArrayList<Vertex> convertToArrayList(LinkedList pList){
