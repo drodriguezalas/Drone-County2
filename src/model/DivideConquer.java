4b4d7f5142b2d57728066ee97b60619c8f7163c4
drodriguezalas <drodriguezalas@gmail.com>
2018-05-05 14:38:35 -0600

    DivideConquer encapsulation

--- src/model/DivideConquer.java
+++ src/model/DivideConquer.java
@@ -4,15 +4,28 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class DivideConquer extends Algorithm{
+	//CONSTANTS
 	public static final int PULSE = 2;
 
+	//ATRIBUTES
 	public ArrayList<ArrayList<Trip>> tripBlocks;
 
+	//CONSTRUCTOR
 	public DivideConquer(ArrayList<Trip> pTripList, int pTotalTime)
 	{
 		super(pTripList, pTotalTime);
 		this.tripBlocks = new ArrayList<>();
-	}		
+	}	
+
+	//METHODS
+	@Override
+	public void generateSimulatorTimeline(ArrayList<Trip> pTripList) {
+		divide();
+		for(ArrayList<Trip> tripBlock : this.tripBlocks)
+		{
+			this.conquer(tripBlock);
+		}
+	}
 
 	private void divide() 
 	{		
@@ -20,26 +33,15 @@ public class DivideConquer extends Algorithm{
 		{
 			if(this.tripBlocks.isEmpty())
 			{
-				ArrayList<Trip> fistTripList = new ArrayList<>();
-				fistTripList.add(trip);
-				this.tripBlocks.add(tripList);
+				createNewTripBlock(trip);
 			}
 			else
 			{
 				boolean tripFound = false;
-				for(ArrayList<Trip> tripBlock : this.tripBlocks)
-				{
-					if(tripBlock.get(0).equals(trip))
-					{
-						tripFound = true;
-						tripBlock.add(trip);
-					}
-				}
+				lookForTripBlock(trip, tripFound);
 				if(!tripFound)
 				{
-					ArrayList<Trip> newTripList = new ArrayList<>();
-					newTripList.add(trip);
-					this.tripBlocks.add(newTripList);
+					createNewTripBlock(trip);
 				}
 			}
 		}
@@ -52,27 +54,33 @@ public class DivideConquer extends Algorithm{
 			writeBlockAtTime(0, pBlock);
 		}
 		else
-		{
-			Integer newMoment = 0;
-			for(Integer keyMoment : this.simulatorTimeline.keySet())
-			{
-				boolean collisionFound = false;
-				for(Trip trip : this.simulatorTimeline.get(keyMoment))
-				{
-					if(checkCollision(trip, pBlock.get(0)))
-					{
-						collisionFound = true;
-					}
-				}
-				if(!collisionFound)
-				{
-					writeBlockAtTime(keyMoment, pBlock);
-					return;
-				}
-				newMoment = keyMoment + PULSE;
-			}
-			writeBlockAtTime(newMoment, pBlock);
+		{			
+			mergeBlock(pBlock);
 		}
+	}
+
+	public void mergeBlock(ArrayList<Trip> pBlock)
+	{
+		Integer newMoment = 0;
+		for(Integer keyMoment : this.simulatorTimeline.keySet())
+		{
+			boolean collisionFound = false;
+			for(Trip trip : this.simulatorTimeline.get(keyMoment))
+			{
+				if(checkCollision(trip, pBlock.get(0)))
+				{
+					collisionFound = true;
+					break;
+				}
+			}
+			if(!collisionFound)
+			{
+				writeBlockAtTime(keyMoment, pBlock);
+				return;
+			}
+			newMoment = keyMoment + PULSE;
+		}
+		writeBlockAtTime(newMoment, pBlock);
 	}
 
 	public boolean checkCollision(Trip pTrip1, Trip pTrip2)
@@ -89,6 +97,25 @@ public class DivideConquer extends Algorithm{
 		}
 		return false;
 	}
+
+	public void lookForTripBlock(Trip pTrip, boolean pTripFound)
+	{
+		for(ArrayList<Trip> tripBlock : this.tripBlocks)
+		{
+			if(tripBlock.get(0).equals(pTrip))
+			{
+				pTripFound = true;
+				tripBlock.add(pTrip);
+			}
+		}
+	}
+
+	public void createNewTripBlock(Trip pTrip)
+	{
+		ArrayList<Trip> newTripBlock = new ArrayList<>();
+		newTripBlock.add(pTrip);
+		this.tripBlocks.add(newTripBlock);
+	}			
 
 	public void writeBlockAtTime(Integer pStartTime, ArrayList<Trip> pTripBlock)
 	{
@@ -108,16 +135,7 @@ public class DivideConquer extends Algorithm{
 				pStartTime = pStartTime + PULSE;
 			}
 		}
-	}		
-
-	@Override
-	public void generateSimulatorTimeline(ArrayList<Trip> pTripList) {
-		divide();
-		for(ArrayList<Trip> tripBlock : this.tripBlocks)
-		{
-			this.conquer(tripBlock);
-		}
-	}
+	}			
 
 	@Override
 	public Hashtable<Trip, ArrayList<Integer>> generateHashTiming(ArrayList<Trip> pTripList) {
